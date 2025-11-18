e16d6995755c1e6f275f8e8c186473014f9bf666
David Tweedle <david.aig.tweedle@gmail.com>
2025-11-17 19:15:45 -0500

    added explicit communication

--- submissions/self_tuning/lwrkap/submission.py
+++ submissions/self_tuning/lwrkap/submission.py
@@ -110,9 +110,13 @@ def train_step(workload,
     grad_fn = jax.value_and_grad(_loss_fn, has_aux=True)
     (summed_loss, (n_valid_examples, new_model_state)), grad = grad_fn(
           current_param_container)
+    summed_loss = jax.lax.psum(summed_loss, axis_name='batch')
+    total_n_valid_examples jax.lax.psum(n_valid_examples, axis_name='batch')
+    grad = jax.lax.pmean(grad, axis_name='batch')
     # Get correct global mean loss and grad.
-    loss = summed_loss / n_valid_examples
-    grad = jax.tree.map(lambda x: x / n_valid_examples, grad)
+    loss = summed_loss / total_n_valid_examples
+    grad = jax.tree.map(lambda x: x / total_n_valid_examples, grad)
+    grad = jax.lax.pmean(grad, axis_name='batch')
 
     grad_norm = jnp.sqrt(
           sum(jnp.sum(g**2) for g in jax.tree_util.tree_leaves(grad)))
