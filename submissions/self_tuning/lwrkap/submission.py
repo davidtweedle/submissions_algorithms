e06f8c45623f34a5520f11cbc0f793793a9d723d
David Tweedle <david.aig.tweedle@gmail.com>
2025-11-19 19:35:57 -0500

    cifar batch size

--- submissions/self_tuning/lwrkap/submission.py
+++ submissions/self_tuning/lwrkap/submission.py
@@ -266,7 +266,7 @@ def get_batch_size(workload_name):
     elif workload_name == 'mnist':
         return 16
     elif workload_name == 'cifar':
-        return 1024
+        return 128
     else:
         raise ValueError(f'Unsupported workload name: {workload_name}.')
 
